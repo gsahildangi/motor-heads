b4ac14abb380b4dfc17ac38f35f1be696ec64f17
Sahil Dangi <gsahil0508@gmail.com>
2025-10-15 05:35:31 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Background from './Components/Background/Background'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
 
 const App = () => {
    
@@ -8,14 +9,33 @@ const App = () => {
     {text1 :"Dive into" , text2 :"what you love"},
     {text1 :"Indulge" , text2 :"your passions"},
     {text1 :"Give in to" , text2 :"your passions"},
+    {text1 :"Dive into" , text2 :"what you love"},
+    {text1 :"Indulge" , text2 :"your passions"},
   ]
 
   const [heroCount, setHeroCount] = useState(0);
   const [playStatus , setPlayStatus] = useState(false);
+
+    // useEffect(()=> {
+    //   //  console.log("hy");
+    //     setInterval(() => {
+    //         //  console.log("hy from inside");
+    //         setHeroCount((count) =>{return count===4?0:count+1} )
+    //     },3000);  
+    //  },[])
+
+
   return (
     <div>
       <Background  playStatus = {playStatus} heroCount ={heroCount} />
       <Navbar/> 
+      <Hero 
+          setPlayStatus={setPlayStatus}
+          heroData={heroData[heroCount]}
+          heroCount={heroCount}
+          setHeroCount={setHeroCount}
+          playStatus={playStatus}
+      />
     </div>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,34 @@
 import './Hero.css'
+import arrowBtn from '../../assets/arrow_btn.png'
+import playIcon from '../../assets/play_icon.png'
+import pauseIcon from '../../assets/pause_icon.png'
 
-const Hero = () => {
+
+const Hero = ({heroData,heroCount,playStatus,setPlayStatus,setHeroCount}) => {
 
     return (
-        <div>
-
+        <div className='hero'> 
+            <div className="hero-text">
+                <p>{heroData.text1}</p>
+                <p>{heroData.text2}</p>
+            </div>
+            <div className="hero-explore">
+                <p>Explore More Cars</p>
+                <img src={arrowBtn} alt="Left Pointing Arrow" />
+            </div>
+            <div className="hero-dot-play">
+                <ul className='hero-dots'>
+                    <li onClick={() =>setHeroCount(0)} className={heroCount===0?'hero-dot orange':'hero-dot'}></li>
+                    <li onClick={() =>setHeroCount(1)} className={heroCount===1?'hero-dot orange':'hero-dot'}></li>
+                    <li onClick={() =>setHeroCount(2)} className={heroCount===2?'hero-dot orange':'hero-dot'}></li>
+                    <li onClick={() =>setHeroCount(3)} className={heroCount===3?'hero-dot orange':'hero-dot'}></li>
+                    <li onClick={() =>setHeroCount(4)} className={heroCount===4?'hero-dot orange':'hero-dot'}></li>
+                </ul>
+                <div className="hero-play">
+                    <img onClick={() => setPlayStatus(!playStatus)} src={playStatus?pauseIcon:playIcon} alt='Play/Pause Button'></img>
+                    <p>See the video</p>
+                </div>
+            </div>
         </div>
     )
 }
