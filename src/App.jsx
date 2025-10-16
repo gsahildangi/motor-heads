e53607b6e0527135a34a152b78ab8a8f6726d9cd
Sahil Dangi <gsahil0508@gmail.com>
2025-10-17 01:51:54 +0530

    Final Push

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,44 @@ import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 
 const App = () => {
-   
+
   let heroData = [
-    {text1 :"Dive into" , text2 :"what you love"},
-    {text1 :"Indulge" , text2 :"your passions"},
-    {text1 :"Give in to" , text2 :"your passions"},
-    {text1 :"Dive into" , text2 :"what you love"},
-    {text1 :"Indulge" , text2 :"your passions"},
-  ]
+    { text1: "Explore", text2: "what moves you" },
+    { text1: "Uncover", text2: "your next obsession" },
+    { text1: "Lose yourself in", text2: "what you love" },
+    { text1: "Fuel", text2: "your curiosity" },
+    { text1: "Follow", text2: "your passions" },
+    // { text1: "Embrace", text2: "what excites you" },
+    // { text1: "Discover", text2: "your next favorite thing" },
+    // { text1: "Get lost in", text2: "what you love" },
+    // { text1: "Experience", text2: "the things you adore" },
+    // { text1: "Satisfy", text2: "your curiosity" }
+  ];
 
   const [heroCount, setHeroCount] = useState(0);
-  const [playStatus , setPlayStatus] = useState(false);
+  const [playStatus, setPlayStatus] = useState(true);
 
-    // useEffect(()=> {
-    //   //  console.log("hy");
-    //     setInterval(() => {
-    //         //  console.log("hy from inside");
-    //         setHeroCount((count) =>{return count===4?0:count+1} )
-    //     },3000);  
-    //  },[])
+  // useEffect(()=> {
+  //   //  console.log("hy");
+  //    const interval = setInterval(() => {
+  //         //  console.log("hy from inside");
+  //         setHeroCount((prevCount) => {return prevCount===4?0:prevCount+1} )
+  //     },3000);  
+
+  //     return () => clearInterval(interval);
+  //  },[])
 
 
   return (
     <div>
-      <Background  playStatus = {playStatus} heroCount ={heroCount} />
-      <Navbar/> 
-      <Hero 
-          setPlayStatus={setPlayStatus}
-          heroData={heroData[heroCount]}
-          heroCount={heroCount}
-          setHeroCount={setHeroCount}
-          playStatus={playStatus}
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
       />
     </div>
   )
